d524ac3156bd40855d60f4fec51138923d052689
Lars Haider <larshaider@yahoo.de>
2024-04-12 19:37:50 +0200

    removed navigation from app module

--- app/src/main/java/de/lhaider/yodo/ui/navigation/Screen.kt
+++
@@ -1,7 +0,0 @@
-package de.lhaider.yodo.ui.navigation
-
-sealed class Screen(val route: String) {
-    data object Home : Screen("home")
-    data object DemonsSouls : Screen("demons_souls")
-    data object DarkSouls3 : Screen("dark_souls_3")
-}

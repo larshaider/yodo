c28da679c76a9516a6fc6069a29ece484ccc6d22
Lars Haider <larshaider@yahoo.de>
2025-04-06 23:42:02 +0200

    added enemies, location & game for dark souls

--- feature/src/main/java/de/lhaider/yodo/feature/dark_souls/enemy/DSBoss.kt
+++ feature/src/main/java/de/lhaider/yodo/feature/dark_souls/enemy/DSBoss.kt
@@ -1,4 +1,40 @@
 package de.lhaider.yodo.feature.dark_souls.enemy
 
-class DSBoss {
+import de.lhaider.yodo.common.ui_text.UIText
+import de.lhaider.yodo.core.enemy.Boss
+import de.lhaider.yodo.feature.R
+
+sealed class DSBoss(
+    enemyId: String,
+    override val name: UIText = UIText.Resource(R.string.undefined)
+) : Boss {
+    override val identifier = "ds_boss_$enemyId"
+    override val pointsForKill = 5
 }
+
+data object ArtoriasTheAbysswalker : DSBoss("artorias_the_abysswalker", UIText.Resource(R.string.ds_artorias_the_abysswalker))
+data object AsylumDemon : DSBoss("asylum_demon", UIText.Resource(R.string.ds_asylum_demon))
+data object BedOfChaos : DSBoss("bed_of_chaos", UIText.Resource(R.string.ds_bed_of_chaos))
+data object BellGargoyles : DSBoss("bell_gargoyles", UIText.Resource(R.string.ds_bell_gargoyles))
+data object CapraDemon : DSBoss("capra_demon", UIText.Resource(R.string.ds_capra_demon))
+data object CeaselessDischarge : DSBoss("ceaseless_discharge", UIText.Resource(R.string.ds_ceaseless_discharge))
+data object CentipedeDemon : DSBoss("centipede_demon", UIText.Resource(R.string.ds_centipede_demon))
+data object DemonFiresage : DSBoss("demon_firesage", UIText.Resource(R.string.ds_demon_firesage))
+data object FourKings : DSBoss("four_kings", UIText.Resource(R.string.ds_four_kings))
+data object GapingDragon : DSBoss("gaping_dragon", UIText.Resource(R.string.ds_gaping_dragon))
+data object GwynLordOfCinder : DSBoss("gwyn_lord_of_cinder", UIText.Resource(R.string.ds_gwyn_lord_of_cinder))
+data object DarkSunGwyndolin : DSBoss("dark_sun_gwyndolin", UIText.Resource(R.string.ds_dark_sun_gwyndolin))
+data object IronGolem : DSBoss("iron_golem", UIText.Resource(R.string.ds_iron_golem))
+data object BlackDragonKalameet : DSBoss("black_dragon_kalameet", UIText.Resource(R.string.ds_black_dragon_kalameet))
+data object ManusFatherOfTheAbyss : DSBoss("manus_father_of_the_abyss", UIText.Resource(R.string.ds_manus_father_of_the_abyss))
+data object MoonlightButterfly : DSBoss("moonlight_butterfly", UIText.Resource(R.string.ds_moonlight_butterfly))
+data object GravelordNito : DSBoss("gravelord_nito", UIText.Resource(R.string.ds_gravelord_nito))
+data object OrnsteinAndSmough : DSBoss("ornstein_and_smough", UIText.Resource(R.string.ds_ornstein_smough))
+data object Pinwheel : DSBoss("pinwheel", UIText.Resource(R.string.ds_pinwheel))
+data object CrossbreedPriscilla : DSBoss("crossbreed_priscilla", UIText.Resource(R.string.ds_crossbreed_priscilla))
+data object ChaosWitchQuelaag : DSBoss("chaos_witch_quelaag", UIText.Resource(R.string.ds_chaos_witch_quelaag))
+data object SanctuaryGuardian : DSBoss("sanctuary_guardian", UIText.Resource(R.string.ds_sanctuary_guardian))
+data object SeathTheScaleless : DSBoss("seath_the_scaleless", UIText.Resource(R.string.ds_seath_the_scaleless))
+data object GreatGreyWolfSif : DSBoss("great_grey_wolf_sif", UIText.Resource(R.string.ds_great_grey_wolf_sif))
+data object StrayDemon : DSBoss("stray_demon", UIText.Resource(R.string.ds_stray_demon))
+data object TaurusDemon : DSBoss("taurus_demon", UIText.Resource(R.string.ds_taurus_demon))
--- feature/src/main/java/de/lhaider/yodo/feature/dark_souls/game/DarkSouls.kt
+++ feature/src/main/java/de/lhaider/yodo/feature/dark_souls/game/DarkSouls.kt
@@ -2,8 +2,33 @@ package de.lhaider.yodo.feature.dark_souls.game
 
 import de.lhaider.yodo.common.ui_text.UIText
 import de.lhaider.yodo.core.game.Game
-import de.lhaider.yodo.core.location.Location
 import de.lhaider.yodo.feature.R
+import de.lhaider.yodo.feature.dark_souls.location.Abyss
+import de.lhaider.yodo.feature.dark_souls.location.AnorLondo
+import de.lhaider.yodo.feature.dark_souls.location.AshLake
+import de.lhaider.yodo.feature.dark_souls.location.Blighttown
+import de.lhaider.yodo.feature.dark_souls.location.Catacombs
+import de.lhaider.yodo.feature.dark_souls.location.ChasmOfTheAbyss
+import de.lhaider.yodo.feature.dark_souls.location.CrystalCave
+import de.lhaider.yodo.feature.dark_souls.location.DarkrootBasin
+import de.lhaider.yodo.feature.dark_souls.location.DarkrootGarden
+import de.lhaider.yodo.feature.dark_souls.location.DemonRuins
+import de.lhaider.yodo.feature.dark_souls.location.Depths
+import de.lhaider.yodo.feature.dark_souls.location.DukesArchive
+import de.lhaider.yodo.feature.dark_souls.location.KilnOfTheFirstFlame
+import de.lhaider.yodo.feature.dark_souls.location.LostIzalith
+import de.lhaider.yodo.feature.dark_souls.location.NewLondoRuins
+import de.lhaider.yodo.feature.dark_souls.location.OolacileTownship
+import de.lhaider.yodo.feature.dark_souls.location.PaintedWorldOfAriamis
+import de.lhaider.yodo.feature.dark_souls.location.QuelaagsDomain
+import de.lhaider.yodo.feature.dark_souls.location.RoyalWood
+import de.lhaider.yodo.feature.dark_souls.location.SanctuaryGarden
+import de.lhaider.yodo.feature.dark_souls.location.SensFortress
+import de.lhaider.yodo.feature.dark_souls.location.TombOfGiants
+import de.lhaider.yodo.feature.dark_souls.location.UndeadAsylum
+import de.lhaider.yodo.feature.dark_souls.location.UndeadBurg
+import de.lhaider.yodo.feature.dark_souls.location.UndeadParish
+import de.lhaider.yodo.feature.dark_souls.location.ValleyOfDrakes
 
 class DarkSouls : Game {
 
@@ -11,5 +36,32 @@ class DarkSouls : Game {
 
     override val name = UIText.Resource(R.string.dark_souls)
 
-    override val locations = emptyList<Location>()
+    override val locations = listOf(
+        UndeadAsylum,
+        UndeadBurg,
+        UndeadParish,
+        DarkrootGarden,
+        DarkrootBasin,
+        Depths,
+        Blighttown,
+        QuelaagsDomain,
+        AshLake,
+        ValleyOfDrakes,
+        SensFortress,
+        AnorLondo,
+        PaintedWorldOfAriamis,
+        NewLondoRuins,
+        Abyss,
+        DukesArchive,
+        CrystalCave,
+        DemonRuins,
+        LostIzalith,
+        Catacombs,
+        TombOfGiants,
+        KilnOfTheFirstFlame,
+        SanctuaryGarden,
+        RoyalWood,
+        OolacileTownship,
+        ChasmOfTheAbyss
+    )
 }
